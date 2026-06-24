0f9414ca3780c33a427fa1bb4bc1633252de72a4
Kyonshi <155651185+kyonshi0104@users.noreply.github.com>
2026-06-25 01:30:21 +0900

    Adjusted to be compatible with 1.21–1.21.1
    Removed copper lanterns
    Changed to use ResourceLocation

--- src/client/java/com/kyonshi/client/ToggleLanternsClient.java
+++ src/client/java/com/kyonshi/client/ToggleLanternsClient.java
@@ -4,30 +4,21 @@ import com.kyonshi.ModBlocks;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
-import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.block.Block;
 
 @Environment(EnvType.CLIENT)
 public class ToggleLanternsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        // 透過させたいランタンを配列にまとめる
         Block[] translucentBlocks = {
                 ModBlocks.UNLIT_LANTERN,
-                ModBlocks.UNLIT_SOUL_LANTERN,
-                ModBlocks.UNLIT_COPPER_LANTERN,
-                ModBlocks.UNLIT_EXPOSED_COPPER_LANTERN,
-                ModBlocks.UNLIT_WEATHERED_COPPER_LANTERN,
-                ModBlocks.UNLIT_OXIDIZED_COPPER_LANTERN,
-                ModBlocks.UNLIT_WAXED_COPPER_LANTERN,
-                ModBlocks.UNLIT_WAXED_EXPOSED_COPPER_LANTERN,
-                ModBlocks.UNLIT_WAXED_WEATHERED_COPPER_LANTERN,
-                ModBlocks.UNLIT_WAXED_OXIDIZED_COPPER_LANTERN
+                ModBlocks.UNLIT_SOUL_LANTERN
         };
 
         for (Block block : translucentBlocks) {
-            BlockRenderLayerMap.putBlock(block, ChunkSectionLayer.TRANSLUCENT);
+            BlockRenderLayerMap.INSTANCE.putBlock(block, RenderType.translucent());
         }
     }
 }
--- src/main/java/com/kyonshi/ModBlocks.java
+++ src/main/java/com/kyonshi/ModBlocks.java
@@ -4,7 +4,7 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTabs;
@@ -14,13 +14,12 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.LanternBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import org.jetbrains.annotations.NotNull;
 
 public class ModBlocks {
 
     private static Block registerLantern(String name) {
-        ResourceKey<@NotNull Block> blockKey = ResourceKey.create(Registries.BLOCK, Identifier.fromNamespaceAndPath(ToggleLanterns.MOD_ID, name));
-        ResourceKey<@NotNull Item> itemKey = ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(ToggleLanterns.MOD_ID, name));
+        ResourceKey<Block> blockKey = ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(ToggleLanterns.MOD_ID, name));
+        ResourceKey<Item> itemKey = ResourceKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(ToggleLanterns.MOD_ID, name));
 
         Block block = Registry.register(
                 BuiltInRegistries.BLOCK,
@@ -28,56 +27,25 @@ public class ModBlocks {
                 new LanternBlock(BlockBehaviour.Properties.of()
                         .sound(SoundType.LANTERN)
                         .strength(3.5F)
-                        .noOcclusion()
-                        .setId(blockKey))
+                        .noOcclusion())
         );
 
-        // ブロックアイテムも同時に登録
         Registry.register(
                 BuiltInRegistries.ITEM,
                 itemKey,
-                new BlockItem(block, new Item.Properties().setId(itemKey).useBlockDescriptionPrefix())
+                new BlockItem(block, new Item.Properties())
         );
 
         return block;
     }
 
-    // 鉄ランタン
     public static final Block UNLIT_LANTERN = registerLantern("unlit_lantern");
     public static final Block UNLIT_SOUL_LANTERN = registerLantern("unlit_soul_lantern");
 
-    // 銅ランタン
-    public static final Block UNLIT_COPPER_LANTERN = registerLantern("unlit_copper_lantern");
-    public static final Block UNLIT_EXPOSED_COPPER_LANTERN = registerLantern("unlit_exposed_copper_lantern");
-    public static final Block UNLIT_WEATHERED_COPPER_LANTERN = registerLantern("unlit_weathered_copper_lantern");
-    public static final Block UNLIT_OXIDIZED_COPPER_LANTERN = registerLantern("unlit_oxidized_copper_lantern");
-
-    // 銅ランタン
-    public static final Block UNLIT_WAXED_COPPER_LANTERN = registerLantern("unlit_waxed_copper_lantern");
-    public static final Block UNLIT_WAXED_EXPOSED_COPPER_LANTERN = registerLantern("unlit_waxed_exposed_copper_lantern");
-    public static final Block UNLIT_WAXED_WEATHERED_COPPER_LANTERN = registerLantern("unlit_waxed_weathered_copper_lantern");
-    public static final Block UNLIT_WAXED_OXIDIZED_COPPER_LANTERN = registerLantern("unlit_waxed_oxidized_copper_lantern");
-
-
     public static void initialize() {
         ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register((itemGroup) -> {
-
-            // 鉄ランタン
             itemGroup.addAfter(Items.LANTERN, UNLIT_LANTERN.asItem());
             itemGroup.addAfter(Items.SOUL_LANTERN, UNLIT_SOUL_LANTERN.asItem());
-
-            // 銅ランタン
-            itemGroup.addAfter(Items.COPPER_LANTERN.unaffected(), UNLIT_COPPER_LANTERN.asItem());
-
-            itemGroup.addAfter(Items.COPPER_LANTERN.exposed(), UNLIT_EXPOSED_COPPER_LANTERN.asItem());
-            itemGroup.addAfter(Items.COPPER_LANTERN.weathered(), UNLIT_WEATHERED_COPPER_LANTERN.asItem());
-            itemGroup.addAfter(Items.COPPER_LANTERN.oxidized(), UNLIT_OXIDIZED_COPPER_LANTERN.asItem());
-
-            // waxed
-            itemGroup.addAfter(Items.COPPER_LANTERN.waxed(), UNLIT_WAXED_COPPER_LANTERN.asItem());
-            itemGroup.addAfter(Items.COPPER_LANTERN.waxedExposed(), UNLIT_WAXED_EXPOSED_COPPER_LANTERN.asItem());
-            itemGroup.addAfter(Items.COPPER_LANTERN.waxedWeathered(), UNLIT_WAXED_WEATHERED_COPPER_LANTERN.asItem());
-            itemGroup.addAfter(Items.COPPER_LANTERN.waxedOxidized(), UNLIT_WAXED_OXIDIZED_COPPER_LANTERN.asItem());
         });
     }
 }
